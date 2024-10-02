d97afd57b87fe3596c56d96d4ee9873fe204a586
sonicnos <107223123+sonicnos@users.noreply.github.com>
2024-10-02 19:40:00 +0200

    make dark mode and sidebar

--- src/app/dashboardWrapper.tsx
+++ src/app/dashboardWrapper.tsx
@@ -1,18 +1,44 @@
-import React from "react";
-import NavBar from "../components/NavBar";
-import Sidebar from "../components/Sidebar";
+"use client";
 
-const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+import React, { useEffect } from "react";
+import Navbar from "@/components/NavBar";
+import Sidebar from "@/components/Sidebar";
+import StoreProvider, { useAppSelector } from "./redux";
+
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed,
+  );
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  });
+
   return (
     <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
       <Sidebar />
       <main
-        className={`dark:bg-dark-bg flex w-full flex-col bg-gray-50 md:pl-64`}
+        className={`dark:bg-dark-bg flex w-full flex-col bg-gray-50 ${
+          isSidebarCollapsed ? "" : "md:pl-64"
+        }`}
       >
-        <NavBar />
+        <Navbar />
         {children}
       </main>
     </div>
+  );
+};
+
+const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <StoreProvider>
+      <DashboardLayout>{children}</DashboardLayout>
+    </StoreProvider>
   );
 };
 
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,8 +1,12 @@
 "use client";
 
-import { LockIcon } from "lucide-react";
+import { Home, LockIcon } from "lucide-react";
+import { LucideIcon } from "lucide-react";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 import React, { useState } from "react";
+import { useAppDispatch, useAppSelector } from "@/app/redux";
+import Link from "next/link";
 
 const Sidebar = () => {
   const [showProject, setShowProjects] = useState(true);
@@ -32,8 +36,54 @@ const Sidebar = () => {
           </div>
         </div>
         {/* NavBar Links */}
+        <nav className="z-10 w-full">
+          <SidebarLink icon={Home} label="Home" href="/" />
+        </nav>
       </div>
     </div>
+  );
+};
+
+interface SidebarLinkProps {
+  href: string;
+  icon: LucideIcon;
+  label: string;
+  isCollapsed: boolean;
+}
+
+const SidebarLink = ({
+  href,
+  icon: Icon,
+  label,
+  //isCollapsed,
+}: SidebarLinkProps) => {
+  const pathname = usePathname();
+  const isActive =
+    pathname === href || (pathname === "/" && href === "/dashboard");
+  const screenWidth = window.innerWidth;
+
+  const dispatch = useAppDispatch();
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed,
+  );
+
+  return (
+    <Link href={href} className="w-full">
+      <div
+        className={`relative flex cursor-pointer items-center gap-3 transition-colors hover:bg-gray-100 dark:bg-black dark:hover:bg-gray-700 ${
+          isActive ? "bg-gray-100 text-white dark:bg-gray-600" : ""
+        }`}
+      >
+        {isActive && (
+          <div className="bg-bleu-200 absolute left-0 top-0 h-[100%] w-[5px]" />
+        )}
+
+        <Icon className="h-6 w-6 text-gray-800 dark:text-gray-100" />
+        <span className={`font-medium text-gray-800 dark:text-gray-100`}>
+          {label}
+        </span>
+      </div>
+    </Link>
   );
 };
 
