87ad5f76e2179fbdf2ef5f6be75508cb60e5d846
sonicnos <107223123+sonicnos@users.noreply.github.com>
2024-10-13 07:06:10 +0200

    modal and form for each categories

--- src/app/projects/[id]/ProjectHeader.tsx
+++ src/app/projects/[id]/ProjectHeader.tsx
@@ -1,7 +1,16 @@
 "use client";
 import Header from "@/components/Header";
-import { Clock, Filter, Grid3x3, List, Share2, Table } from "lucide-react";
+import {
+  Clock,
+  Filter,
+  Grid3x3,
+  List,
+  PlusSquare,
+  Share2,
+  Table,
+} from "lucide-react";
 import { useState } from "react";
+import ModalNewProject from "../ModalNewProject";
 
 type Props = {
   activeTab: string;
@@ -12,9 +21,22 @@ const ProjectHeader = ({ activeTab, setActiveTab }: Props) => {
   const [isModalNewProjectOpen, setIsModalNewTaskOpen] = useState(false);
   return (
     <div className="px-4 xl:px-6">
-      {/* MODAL NEW PROJECT */}
+      <ModalNewProject
+        isOpen={isModalNewProjectOpen}
+        onClose={() => setIsModalNewTaskOpen(false)}
+      />
       <div className="pb-6 pt-6 lg:pb-4 lg:pt-8">
-        <Header name="Product Design Development" />
+        <Header
+          name="Product Design Development"
+          buttonComponent={
+            <button
+              className="flex items-center rounded-md bg-blue-primary px-3 py-2 text-white hover:bg-blue-600"
+              onClick={() => setIsModalNewTaskOpen(true)}
+            >
+              <PlusSquare className="mr-2 h-5 w-5" /> New Boards
+            </button>
+          }
+        />
       </div>
       {/* TABS */}
       <div className="flex flex-wrap-reverse gap-2 border-y border-gray-200 pb-[8px] pt-2 dark:border-stroke-dark md:items-center">
--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -6,6 +6,7 @@ import BoardView from "../BoardView";
 import ListView from "../ListView";
 import Timeline from "./TimelineView";
 import TableView from "./TableView";
+import ModalNewTask from "@/components/ModalNewTask";
 
 type Props = {
   params: { id: string };
@@ -16,7 +17,10 @@ const Project = ({ params }: Props) => {
   const [isModalNewTaskOpen, setIsModalNewTaskOpen] = useState<boolean>(false);
   return (
     <div>
-      {/*  Modal New Task */}
+      <ModalNewTask
+        isOpen={isModalNewTaskOpen}
+        onClose={() => setIsModalNewTaskOpen(false)}
+      />
       <ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} />
       {activeTab === "Board" && (
         <BoardView id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
